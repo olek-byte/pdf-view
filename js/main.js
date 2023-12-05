3ee445f9953af3596281c1832fc65d8866d0f2f5
Alex <olekhyzhniak@gmail.com>
2023-12-05 14:35:42 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
+//PDF Rendering page
+
 initPDFViewer = () => {
-  $("#pdfViewerDiv").html("");
+  $("#pdfViewerDiv1").html("");
+  pdfjsLib
+    .getDocument("Offering-Memorandum-For-Sale.pdf")
+    .promise.then((pdfDoc) => {
+      //   console.log(pdfDoc);
+      let pages = pdfDoc._pdfInfo.numPages;
+      for (let i = 1; i < pages; i++) {
+        pdfDoc
+          .getPage(i)
+          .then((page) => {
+            // console.log(page);
+            let pdfCanvas = document.createElement("canvas");
+            let context = pdfCanvas.getContext("2d");
+            let pageViewPort = page.getViewport({ scale: 1 });
+            // console.log(pageViewPort);
+            pdfCanvas.width = pageViewPort.width;
+            pdfCanvas.height = pageViewPort.height;
+            $("#pdfViewerDiv1").append(pdfCanvas);
+            page.render({
+              canvasContext: context,
+              viewport: pageViewPort,
+            });
+          })
+          .catch((pageErr) => {
+            console.log(pdfErr);
+          });
+      }
+    })
+    .catch((pdfErr) => {
+      console.log(pdfErr);
+    });
+
+  $("#pdfViewerDiv2").html("");
   pdfjsLib
     .getDocument("dummy.pdf")
     .promise.then((pdfDoc) => {
@@ -16,7 +50,7 @@ initPDFViewer = () => {
             // console.log(pageViewPort);
             pdfCanvas.width = pageViewPort.width;
             pdfCanvas.height = pageViewPort.height;
-            $("#pdfViewerDiv").append(pdfCanvas);
+            $("#pdfViewerDiv2").append(pdfCanvas);
             page.render({
               canvasContext: context,
               viewport: pageViewPort,
